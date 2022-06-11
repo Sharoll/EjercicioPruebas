e38267376ce885239ed3dbffd97c6a5a83f04bdc
Sharoll <syuliannaaraujo@unicesar.edu.co>
2022-06-10 23:14:06 -0500

    Actualización de la lista de requisitos

--- TestEjercicioPruebasUnitarias/UnitTest1.cs
+++ TestEjercicioPruebasUnitarias/UnitTest1.cs
@@ -7,7 +7,7 @@ namespace TestEjercicioPruebasUnitarias
         public void TestEjercicioPrueba()
         {
             int valor = EjercicioPruebas.Program.serieFibonacci(0);
-            Assert.AreEqual(1, valor);
+            Assert.AreEqual(0, valor);
 
         }
 
@@ -15,6 +15,22 @@ namespace TestEjercicioPruebasUnitarias
         public void TestEjercicioPrueba1()
         {
             int valor = EjercicioPruebas.Program.serieFibonacci(1);
+            Assert.AreEqual(1, valor);
+
+        }
+
+        [TestMethod]
+        public void TestEjercicioPrueba2()
+        {
+            int valor = EjercicioPruebas.Program.serieFibonacci(2);
+            Assert.AreEqual(1, valor);
+
+        }
+
+        [TestMethod]
+        public void TestEjercicioPrueba3()
+        {
+            int valor = EjercicioPruebas.Program.serieFibonacci(100);
             Assert.AreEqual(1, valor);
 
         }
