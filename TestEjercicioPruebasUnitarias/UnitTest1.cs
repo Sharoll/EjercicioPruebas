89f659537b68e006e7fbc79847167412aa664b13
Sharoll <syuliannaaraujo@unicesar.edu.co>
2022-06-10 23:09:05 -0500

    Eliminación de duplicación

--- TestEjercicioPruebasUnitarias/UnitTest1.cs
+++ TestEjercicioPruebasUnitarias/UnitTest1.cs
@@ -6,9 +6,18 @@ namespace TestEjercicioPruebasUnitarias
         [TestMethod]
         public void TestEjercicioPrueba()
         {
+            int valor = EjercicioPruebas.Program.serieFibonacci(0);
+            Assert.AreEqual(1, valor);
+
+        }
+
+        [TestMethod]
+        public void TestEjercicioPrueba1()
+        {
             int valor = EjercicioPruebas.Program.serieFibonacci(1);
             Assert.AreEqual(1, valor);
 
         }
+
     }
 }
